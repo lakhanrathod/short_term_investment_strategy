0e4ef2e780e73b649831b51299112d7c944310b9
Ubuntu <ubuntu@ip-172-31-36-88.ec2.internal>
2023-10-10 15:35:00 +0000

    initial commit

---
+++ my_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('my_app/', views.my_app, name='my_app'),
+]
---
+++ short_term_investment_strategy/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for short_term_investment_strategy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'short_term_investment_strategy.settings')
+
+application = get_asgi_application()
