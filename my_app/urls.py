645693160de826a61434187a0e45e25b1c745647
lakhan rathod <lakhansr1242@gmail.com>
2023-12-25 16:30:49 +0530

    my_view added

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('my_app/', views.my_app, name='my_app'),
+    path('my_app/', views.my_view, name='my_view'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def my_app(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+def my_view(request):
+    return HttpResponse("hello world")
 
